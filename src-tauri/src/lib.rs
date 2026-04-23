99f705548ee61bdc19b0def90233da3bdd049f9f
qdiaps <qdiaps@gmail.com>
2026-04-23 16:28:22 +0300

    feat(daemon): bootstrap cli arguments and structured logging

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,23 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
+pub mod cli;
+pub mod logging;
+
+use cli::AppCli;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
+    logging::setup_logger();
+
+    let args = AppCli::parse_args();
+
+    if args.daemon {
+        tracing::info!(target: "daemon", "Bootstrapping qpeek in Background/Daemon mode...");
+    } else {
+        tracing::info!(target: "ui", "Bootstrapping qpeek in Standalone mode...")
+    }
+
     tauri::Builder::default()
-        .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .plugin(tauri_plugin_shell::init())
+        .invoke_handler(tauri::generate_handler![])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
